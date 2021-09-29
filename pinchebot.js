8823c5a6358b3b196d3e925c77b7831e8c0b7ee0
paradox2345 <86022474+paradox2345@users.noreply.github.com>
2021-09-29 03:50:38 -0500

    Update pinchebot.js
    codigo del boto

--- pinchebot.js
+++ pinchebot.js
@@ -13,3 +13,56 @@ cliente.on('messageCreate', message => {
         console.log("todo bien por ahora);
       })
   })
+
+  
+  cliente.on('voiceStateUpdate', (oldState, newState) => {
+
+
+      const joined =!!newState.channelId;
+
+      const boleano = joined ? newState.channelId : oldState.channelId;
+
+      let voiceChannel = cliente.guilds.cache.get("873421230299033650").channels.cache.get(boleano);
+      const cosi=voiceChannel.members.size
+
+      const mantanga= voiceChannel.parentId;
+
+      if (cosi < 1 && mantanga==="891449034735222795") {
+        console.log("si se borro");
+      voiceChannel.delete();
+      }
+      else {
+        console.log("mantener abierto");
+      }
+
+
+});
+  
+  
+    function alf(primero){
+    const palabras=primero.split(" ", 3);
+    const [first,second,tird]= palabras;
+      if (first=="--@crear" && second < 5 && tird.length < 8) {
+       return  true;
+       }
+   else {
+     console.log("comando incorrecto")
+   }
+ }
+  
+cliente.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix)) return;
+   if (alf(message.content)) {
+     const dividir=message.content.split(" ", 3);
+     const [namae,ichido,sanmae]=dividir;
+    message.guild.channels.create(sanmae, {
+ type: 'GUILD_VOICE',
+ parent: '891449034735222795' ,
+ userLimit:ichido,
+
+})
+
+
+
+  }
+});
